77bf77c334ec94efc4a07ee244a732fed5166534
Victor <victor.morel@etu.u-paris.fr>
2022-05-19 01:47:40 +0200

    Day two done !

--- bin/dTwo.ml
+++ bin/dTwo.ml
@@ -26,5 +26,26 @@ let rec loop1 l c =
     | _-> raise Unknown_operation
   )
 
-let main() = let (x1, y1) = loop1 (read "data-d2.txt") (0, 0) in print_int (x1 * y1)
+
+let forward2 (c: int * int * int) n = let (x, y, aim) = c in (x + n, y + (aim * n), aim)
+
+let down2 (c: int * int * int) n = let (x, y, aim) = c in (x, y, aim + n)
+
+let up2 (c: int * int * int) n = let (x, y, aim) = c in (x, y, aim - n)
+
+let rec loop2 l c = 
+  match l with
+  | [] -> c
+  | h::t -> (
+    match String.split_on_char ' ' h with 
+    | "forward"::s::_ -> loop2 t (forward2 c (int_of_string s))
+    | "down"::s::_ -> loop2 t (down2 c (int_of_string s))
+    | "up"::s::_ -> loop2 t (up2 c (int_of_string s))
+    | _-> raise Unknown_operation
+  )
+
+let main() = let r = read "data-d2.txt" in 
+  let (x1, y1) = loop1 r (0, 0) in print_int (x1 * y1);
+  print_newline();
+  let (x2, y2, _) = loop2 r (0, 0, 0) in print_int (x2 * y2)
   
